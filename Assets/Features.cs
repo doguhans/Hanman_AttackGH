d8ea94374595d1e60d79368cdf651cdce1fe7535
Doğuhan Satıcı <61123620+doguhans@users.noreply.github.com>
2023-09-01 13:45:54 +0300

    First Real Commit
    Notes:
    - Added a 600x600 terrain.
    - Added some terrain tools and texture assets from Asset Store to the project.
    - Carved and painted terrain and added some tree models on top of the  terrain.
    - Added a cube test object as player game object for now.

---
+++ Assets/Features.cs
@@ -0,0 +1,13 @@
+/*
+Camera Rail: Path through the level that the camera follows.
+
+Player Movement: Horizontal and vertical movement.
+
+Shooting: Player shoot bullets which do damage to opponent.
+
+Health: Enemis should travel on paths and be hand placed by the deesigner.
+
+Score: Points are given for kiling enemies.
+
+Game Loop: Upon dying, player restarts the level. 
+*/
